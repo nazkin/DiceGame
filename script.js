c8cb971388195c91225fbfdfc7b9913e37c74a55
nazkin <nazar.kinash93@gmail.com>
2019-11-09 15:30:33 -0500

    The game conditions are initialized and dice-imgs respond to the random number generator

--- script.js
+++ script.js
@@ -11,9 +11,25 @@ var roundScore = 0;
 //Active player 0 - "player 1" and 1- "player 2" --> it will also be used to control the active player 
 var activePlayer = 0;
 //diceRoll variable keeps track of a random number rolled between 1 and 6 representing the dice roll
-var diceRoll = Math.floor((Math.random()*6) + 1);
+var diceRoll; 
 
-//DOM manipulations 
-document.querySelector("#current-" + activePlayer).textContent = diceRoll;
+//********************************DOM manipulations 
+var diceObj = document.querySelector('.dice');
 
-document.querySelector('.dice').style.display = "none";
+document.querySelector("#current-0").textContent = "0";
+document.querySelector("#current-1").textContent = "0";
+document.querySelector("#score-0").textContent = "0";
+document.querySelector("#score-1").textContent = "0";
+diceObj.style.display = 'none';
+
+//Event handler for the 'Roll' button
+var rollBtn = document.querySelector('.btn-roll');
+rollBtn.addEventListener('click', function() {
+    // Random number for the diceroll 
+        diceRoll = Math.floor((Math.random()*6) + 1);
+    //Connect the random number to our dicephotos to display the right dice and change img
+        diceObj.style.display = "block";
+        diceObj.src = "./photos/dice-" + diceRoll + ".png";
+    //Update the round-score if the the dice roll is more than 1 
+
+});
