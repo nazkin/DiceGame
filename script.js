d72d11d60b3ef5393281c624b9921e4690ba02c9
nazkin <nazar.kinash93@gmail.com>
2019-11-09 14:38:44 -0500

    CSS/HTML for the game is created, vanilla JavaScript next

---
+++ script.js
@@ -0,0 +1,4 @@
+//Goal of the game is to get to 100 points first
+//Player can roll as many times per round as he wishes
+//If a player rolls a 1, all of the points gained that round are lost.
+// Next players turn happens when: a)Player rolls a 1 b) Player chooses to stop rolling('HOLD') the dice for that round
