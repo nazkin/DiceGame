e621a3eda3f0f016dceb010dcc8d0f5935d74088
nazkin <nazar.kinash93@gmail.com>
2019-11-09 19:00:11 -0500

    Hold button, New Game Button and Roll button all work, need to only apply a few finishing touches to finish

--- script.js
+++ script.js
@@ -3,17 +3,12 @@
 //If a player rolls a 1, all of the points gained that round are lost.
 // Next players turn happens when: a)Player rolls a 1 b) Player chooses to stop rolling('HOLD') the dice for that round
 
-
 //Create an array to maintain scores for both players 
 var finalScore = [0,0];
 //Create one variable to maintain the score for each round
 var roundScore = 0;
 //Active player 0 - "player 1" and 1- "player 2" --> it will also be used to control the active player 
 var activePlayer = 0;
-//diceRoll variable keeps track of a random number rolled between 1 and 6 representing the dice roll
-var diceRoll; 
-
-//********************************DOM manipulations 
 var diceObj = document.querySelector('.dice');
 
 document.querySelector("#current-0").textContent = "0";
@@ -21,10 +16,21 @@ document.querySelector("#current-1").textContent = "0";
 document.querySelector("#score-0").textContent = "0";
 document.querySelector("#score-1").textContent = "0";
 diceObj.style.display = 'none';
+document.querySelector('#name-0').textContent = "Player 1";
+document.querySelector('#name-1').textContent = "Player 2";
+
+ 
+
+//diceRoll variable keeps track of a random number rolled between 1 and 6 representing the dice roll
+var diceRoll;
+
+//********************************DOM manipulations 
+
+
+
 
 //Event handler for the 'Roll' button
 var rollBtn = document.querySelector('.btn-roll');
-
 rollBtn.addEventListener('click', function() {
     // Random number for the diceroll 
         diceRoll = Math.floor((Math.random()*6) + 1);
@@ -38,19 +44,60 @@ rollBtn.addEventListener('click', function() {
         document.querySelector("#current-" + activePlayer).textContent = roundScore;
     }
     else {
-        //Clear the score for that round 
-        roundScore = 0;
-        document.querySelector('#current-' + activePlayer).textContent = "0"; 
-        //Next player - toggle the active class to so that the styling changes as well
-        if(activePlayer === 0){
-            activePlayer = 1;
-        }
-        else {
-            activePlayer = 0;
-        }
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        diceObj.style.display = 'none';
+        switchPlayer();
     }    
 });
+//Implementing the Hold button that allows a player to save his or her score
+var holdBtn = document.querySelector('.btn-hold');
+holdBtn.addEventListener('click', function() {
+    //Update the final scores 
+    finalScore[activePlayer] += roundScore;
+    //Update the DOM 
+    document.querySelector('#score-' + activePlayer).textContent = finalScore[activePlayer];
 
+    // Check if the winning condition is met
+    if(finalScore[activePlayer]>= 15){
+        //player won
+        document.querySelector('#name-' + activePlayer).textContent = "WINNER";
+        diceObj.style.display = "none";
+        document.querySelector('.player-'+ activePlayer +'-panel').classList.add('winner');
+    }
+    else {
+    //Switch to the next player
+    switchPlayer();
+    }
+});
+
+var newGameBtn = document.querySelector('.btn-new');
+newGameBtn.addEventListener('click', initGame);
+
+
+function initGame() {
+    roundScore = 0;
+    finalScore = [0,0];
+    activePlayer = 0;
+    document.querySelector("#current-0").textContent = "0";
+    document.querySelector("#current-1").textContent = "0";
+    document.querySelector("#score-0").textContent = "0";
+    document.querySelector("#score-1").textContent = "0";
+    diceObj.style.display = 'none';
+    document.querySelector('#name-0').textContent = "Player 1";
+    document.querySelector('#name-1').textContent = "Player 2";
+
+}
+function switchPlayer() {
+    //Clear the score for that round 
+   
+
+    document.querySelector('#current-' + activePlayer).textContent = "0"; 
+    //Next player - toggle the active class to so that the styling changes as well
+    if(activePlayer === 0){
+        activePlayer = 1;
+    }
+    else {
+        activePlayer = 0;
+    }
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    diceObj.style.display = 'none';
+}
